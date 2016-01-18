e8b05a10752e2b6f0ef873ae1f07db0700a6b155
Teodoro777 <teodoro.coluccio@gmail.com>
2016-01-18 13:50:30 +0100

    Aggiunto file intlist.c

---
+++ intlist.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include "intlist.h"
+
+int intlist_push(IntlistPtr list, int value)
+{
+        list->values[list->size] = value;
+        list->size++;
+}  
+
+void intlist_dump(IntlistPtr list)
+{
+	int conta;
+	
+	for (conta=0;conta<15;conta++) {
+		printf("%d",list->values[conta]);
+	}
+}
+
+void intlist_clear(IntlistPtr list)
+{
+	list->size=0;
+}
+
+void intlist_get(IntlistPtr list, int index)
+{
+}
--- intlist.h
+++ intlist.h
@@ -2,15 +2,16 @@
 #define INTLIST_H
 
 struct Intlist {
-  int  values[16];
+  int values[16];
   int size;
 };
 
 typedef struct Intlist Intlist;
 typedef struct Intlist* IntlistPtr;
 
-int intlist_push(IntlistPtr);
-void intlist_dump(IntlistPtr);
-void intlist_clear(IntlistPtr);
+int intlist_push(IntlistPtr list, int value);
+void intlist_dump(IntlistPtr list);
+void intlist_clear(IntlistPtr list);
+void intlist_get(IntlistPtr list, int index);
 
 #endif
