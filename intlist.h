2fe4a7e1df9321ee9368fb833170092a9cb005b7
Teodoro777 <teodoro.coluccio@gmail.com>
2016-01-31 11:56:14 +0100

    Aggiornato nome struct intlist

--- intlist.h
+++ intlist.h
@@ -1,13 +1,13 @@
 #ifndef INTLIST_H
 #define INTLIST_H
 
-struct Intlist {
+struct intlist {
   int values[16];
   int size;
 };
 
-typedef struct Intlist Intlist;
-typedef struct Intlist* IntlistPtr;
+typedef struct intlist Intlist;
+typedef struct intlist* IntlistPtr;
 
 void intlist_push(IntlistPtr list, int value);
 void intlist_dump(IntlistPtr list);
