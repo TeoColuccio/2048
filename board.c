b542753b792db144d60f167314575aef9cc8a394
Teodoro777 <teodoro.coluccio@gmail.com>
2016-02-11 19:22:57 +0100

    Ultimata funzione board_init()

--- board.c
+++ board.c
@@ -14,12 +14,17 @@ Tile* board_get(BoardPtr b, int index)
 void board_init(BoardPtr b)
 {
   int i;
+  int valori;
 
   for (i=0; i<16; i++) {
     b->ptr_tile[i] = NULL;
   }
   
-  intlist_clear(&(b->pos_free));  
+  intlist_clear(&(b->pos_free));
+  
+  for (valori=0; valori<16; valori++) {
+    intlist_push(&(b->pos_free), valori);
+  }
+
   intlist_dump(&(b->pos_free));
 }
-
